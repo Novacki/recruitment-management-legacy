a6502dd91de90ede611330afcd2ebf4f84ad6660
Fernando Novacki Freitas <novackifernando@gmail.com>
2022-09-27 08:52:21 -0300

    adding error partial

--- src/Server/Services/IdentityServer/IdentityServer.Domain/Constants/Errors/UserErrorMessages.cs
+++ src/Server/Services/IdentityServer/IdentityServer.Domain/Constants/Errors/UserErrorMessages.cs
@@ -2,7 +2,7 @@
 {
     public static class UserErrorMessages
     {
-        public static string UserNotAuthenticated = "E-mail ou senha incorreto";
+        public static string UserNotAuthenticated = "E-mail ou senha incorretos";
         public static string UserNotFound = "Usuário não encontrado";
         public static string UserNotCreated = "Não foi possível criar o usuário";
     }
