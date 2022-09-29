61f606cb8ef8750888064ad92990476157489d46
Fernando Novacki Freitas <novackifernando@gmail.com>
2022-09-29 11:54:45 -0300

    remove domain dependecies

--- src/Server/Services/IdentityServer/IdentityServer.API/Settings/Dependencies/Domain/DomainDependencySettings.cs
+++ src/Server/Services/IdentityServer/IdentityServer.API/Settings/Dependencies/Domain/DomainDependencySettings.cs
@@ -1,4 +1,7 @@
-﻿using IdentityServer.Domain.Settings.Dependencies.Services;
+﻿using IdentityServer.Domain.Services.Auth;
+using IdentityServer.Domain.Services.Auth.Interfaces;
+using IdentityServer.Domain.Services.Users;
+using IdentityServer.Domain.Services.Users.Interfaces;
 
 namespace IdentityServer.API.Settings.Dependencies.Domain
 {
@@ -6,6 +9,7 @@ namespace IdentityServer.API.Settings.Dependencies.Domain
     {
         public static IServiceCollection ConfigureDomainServices(this IServiceCollection services) =>
             services
-                .ConfigureDependencyServices();
+                .AddScoped<IAuthService, AuthService>()
+                .AddScoped<IUserService, UserService>();
     }
 }
